34bfd842a38505b9cfa6d7b22f4d8e7698c66974
Karl Adrian Melecio <karlmelecio04@gmail.com>
2024-06-16 07:12:24 +0800

    add dotenv

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,15 +10,14 @@ import {
 } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBgdZLpc4wSTI1HVUml-jbjxfxow2i6pp8',
-  authDomain: 'utak-test-17e1b.firebaseapp.com',
-  projectId: 'utak-test-17e1b',
-  storageBucket: 'utak-test-17e1b.appspot.com',
-  messagingSenderId: '1038745084694',
-  appId: '1:1038745084694:web:58f1f10ae525a3474a6637',
-  measurementId: 'G-98BDFHP9MJ',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
